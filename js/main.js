37aa7ab342a4fb4d383a1c8b7d7a28159cf2ab92
Matej <warehouse.matej@gmail.com>
2020-04-25 21:47:49 +0200

    m-show and framework object

--- js/main.js
+++ js/main.js
@@ -1,8 +1,73 @@
-let root = document.querySelector('[m-data]');
+window.Matt = {
+    directives: {
+        'm-text': (el,value) => {
+            el.innerText = value;
+        },
+        'm-show': (el,value) => {
+            el.style.display = value ? 'block' : 'none'
+        }
+    },
 
-let data = getInitialData();
+    start () {
+        this.root = document.querySelector('[m-data]');
 
-function getInitialData() {
-    let dataString = root.getAttribute('m-data');
-    return eval('(`${dataString}`)');
+        this.rawData = this.getInitialData();
+
+        this.data = this.observe(this.rawData);
+
+        this.registerListeners();
+        this.refreshDom();
+    },
+
+    getInitialData() {
+        let dataString = this.root.getAttribute('m-data');
+        return eval(`(${dataString})`);
+    },
+
+    observe(rawData) {
+        let self = this;
+        return new Proxy(rawData,{
+            set(target, p, value) {
+                target[p] = value;
+
+                self.refreshDom();
+            }
+        });
+    },
+
+    registerListeners() {
+        this.walkDom(this.root, el => {
+            Array.from(el.attributes).forEach(attribute => {
+                if(!attribute.name.startsWith('@')) { return }
+                let event = attribute.name.replace('@','');
+
+                el.addEventListener(event,() => {
+                    eval(`with (this.data) (${attribute.value})`);
+                })
+            })
+        });
+    },
+
+    refreshDom() {
+        this.walkDom(this.root, el => {
+            Array.from(el.attributes).forEach(attribute => {
+                if(!Object.keys(this.directives).includes(attribute.name)) { return }
+
+                this.directives[attribute.name](el,eval(`with (this.data) (${attribute.value})`))
+            })
+        })
+    },
+
+    walkDom(el, callback) {
+        callback(el);
+
+        el = el.firstElementChild;
+
+        while (el) {
+            this.walkDom(el,callback);
+            el = el.nextElementSibling;
+        }
+    }
 }
+
+window.Matt.start();
